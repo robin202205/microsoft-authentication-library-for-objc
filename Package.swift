2c929ab8d5efbc07f1234f1ae3129ccb4959b394
Bin Luo <bluo@sequoiacap.com>
2022-05-26 18:27:18 +0800

    update

--- Package.swift
+++ Package.swift
@@ -13,6 +13,9 @@ let package = Package(
           targets: ["MSAL"]),
   ],
   targets: [
-      .binaryTarget(name: "MSAL", url: "https://github.com/AzureAD/microsoft-authentication-library-for-objc/releases/download/1.1.26/MSAL.zip", checksum: "7430bc49be821cac9e459ef47470316dd8cc9f600b92e5ffbad39bb81254f3d2")
+    .target(
+        name: "Kingfisher",
+        path: "MSAL/src"
+    )
   ]
 )
